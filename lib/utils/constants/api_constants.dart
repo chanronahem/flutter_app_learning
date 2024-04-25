72c9f74af71428559509a6bba8b38cbe93d8d248
Chanrona Hem <103235743+chanronahem@users.noreply.github.com>
2024-04-25 16:29:20 +0700

    init project with flash screen, onboarding screen & login screen

---
+++ lib/utils/constants/api_constants.dart
@@ -0,0 +1,3 @@
+/* LIST OF Constants used in APIs */
+
+const String tSecretAPIKey = "cwt_live_asdd5gg3has33uh239874h23erfu2";
---
+++ lib/utils/constants/image_strings.dart
@@ -0,0 +1,14 @@
+class TImages {
+  // App Logos
+  static const String darkAppLogo = "assets/logos/h-splash-logo.png";
+  static const String lightAppLogo = "assets/logos/h-splash-logo.png";
+
+  // Social Logos
+  static const String google = "assets/logos/google-icon.png";
+  static const String facebook = "assets/logos/facebook-icon.png";
+
+  // OnBoarding Images
+  static const String onBoardingImage1 = "assets/images/on_boarding_images/choose-product.gif";
+  static const String onBoardingImage2 = "assets/images/on_boarding_images/select-payment-method.gif";
+  static const String onBoardingImage3 = "assets/images/on_boarding_images/delivery.gif";
+}
