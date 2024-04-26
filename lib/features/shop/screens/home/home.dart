991b8b2f396c9dee0b236b8952c86fc4db60f73e
Chanrona Hem <103235743+chanronahem@users.noreply.github.com>
2024-04-26 15:59:16 +0700

    appbar

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,18 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app_learning/features/shop/screens/home/widgets/home_appbar.dart';
 
-import '../../../../common/custom_shapes/containers/primary_header_container.dart';
+import '../../../../common/widgets/custom_shapes/containers/primary_header_container.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: SingleChildScrollView(
         child: Column(
           children: [
             TPrimaryHeaderContainer(
-                child: Container(),
+              child: Column(
+                children: [
+                  /// Appbar
+                  THomeAppBar(),
+
+                  /// Searchbar
+
+                  /// Categories
+
+                ],
+              ),
             ),
           ],
         ),
